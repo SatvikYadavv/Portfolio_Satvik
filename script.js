0676b1fe1cc7d928f5c2961ca518181c17efffb6
SatvikYadavv <yadavsatvik8175@gmail.com>
2025-05-21 15:51:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// import LocomotiveScroll from 'locomotive-scroll';
+
+
+
+
+// const scroll = new LocomotiveScroll();
+// scroll.on("scroll", smooth);
+
+
+
+    document.getElementById('ab').onclick = function () {
+        document.getElementById('abt').scrollIntoView({ behavior: 'smooth' });
+    };
+
+    document.getElementById('edd').onclick = function () {
+        document.getElementById('ed').scrollIntoView({ behavior: 'smooth' });
+    };
+
+    document.getElementById('sss').onclick = function () {
+        document.getElementById('skill').scrollIntoView({ behavior: 'smooth' });
+    };
+    document.getElementById('wex').onclick = function () {
+        document.getElementById('workx').scrollIntoView({ behavior: 'smooth' });
+    };
+    document.getElementById('port').onclick = function () {
+        document.getElementById('proj').scrollIntoView({ behavior: 'smooth' });
+    };
+
+    document.getElementById('cont').onclick = function () {
+        document.getElementById('contc').scrollIntoView({ behavior: 'smooth' });
+    };
+    document.getElementById('contct').onclick = function () {
+        document.getElementById('contc').scrollIntoView({ behavior: 'smooth' });
+    };
+
+    document.getElementById('up-btn').onclick = function () {
+        document.getElementById('index').scrollIntoView({ behavior: 'smooth' });
+    };
+
+
